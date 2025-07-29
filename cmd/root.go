9dec486c36d96dca7b535a6a5046f635f6c9c76b
Kavin <62742678+Kavinraja-G@users.noreply.github.com>
2025-07-29 11:17:18 +0530

    Bump version to v0.1.7

--- cmd/root.go
+++ cmd/root.go
@@ -7,7 +7,7 @@ func NewCmdRoot() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:     "crossplane-docs",
 		Short:   "crossplane-docs - Docs generator for your crossplane resources",
-		Version: "0.1.6",
+		Version: "0.1.7",
 		RunE: func(c *cobra.Command, args []string) error {
 			if err := c.Help(); err != nil {
 				return err
